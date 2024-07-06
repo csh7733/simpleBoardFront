593ca63078609158755a4dda4caef4cba47e5f85
최성현 <popopqo777@gmail.com>
2024-07-07 02:17:22 +0900

    make non-stop

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,17 @@
 REPOSITORY=home/ec2-user/front/zip
 BUILD_DIR=$REPOSITORY/build
 DEPLOY_DIR_FINAL=/var/www/html
-DEPLOY_DIR=/var/www/releases/$TIMESTAMP
 echo "> 새 프론트엔드 애플리케이션 빌드 및 배포"
 
 # 새로운 배포 디렉토리 생성
 TIMESTAMP=$(date +%s)
+DEPLOY_DIR=/var/www/releases/$TIMESTAMP
 sudo mkdir -p $DEPLOY_DIR
 
 # 빌드 파일을 새로운 배포 디렉토리로 복사
 sudo cp -r $BUILD_DIR/* $DEPLOY_DIR/
+
+sudo rm -rf /var/www/html
 
 # 심볼릭 링크 업데이트
 sudo ln -sfn $DEPLOY_DIR $DEPLOY_DIR_FINAL
